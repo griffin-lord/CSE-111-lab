3dd466e51daec4fff4f0c72ed17e84a967af5357
Abdullah Al Tahsin <qwwwrafid9@gmail.com>
2025-03-09 19:54:17 +0600

    Add files via upload

--- Student.java
+++ Student.java
@@ -1,4 +1,22 @@
 public class Student{
   public String name;
-  public int id;
+  public String prog;
+  
+  public Student(String nam,String pro){
+    name=nam;
+    prog=pro;
+  }
+  
+  public void updateName(String na){
+    name=na;
+  }
+  
+  public void updateProgram(String c){
+    prog=c;
+  }
+  
+  public String accessProgram(){
+    return prog;
+  }
 }
+  
--- StudentTester.java
+++ StudentTester.java
@@ -1,24 +1,12 @@
-public class StudentTester{
-  public static void main(String[] args) {
-    Student std1 = new Student();
-    std1.showDetails();
-    System.out.println("1---------------");
-    std1.updateDetails("Alif", 3.99, 12);
-    System.out.println("2---------------");
-    std1.checkScholarshipEligibility();
-    System.out.println("3---------------");
-    std1.showDetails();
-    Student std2 = new Student();
-    std2.updateDetails("Mim", 3.4);
-    Student std3 = new Student();
-    std3.updateDetails("Henry", 3.5, 15, "BBA");
-    System.out.println("5---------------");
-    std2.checkScholarshipEligibility();
-    System.out.println("6---------------");
-    std3.checkScholarshipEligibility();
-    System.out.println("7---------------");
-    std2.showDetails();
-    System.out.println("8---------------");
-    std3.showDetails();
-  }
+public class StudentTester{ 
+   public static void main(String[] args){ 
+       Student s1 = new Student("Harry", "CSE"); 
+       System.out.println(s1.name); 
+       s1.updateName("Harry Potter"); 
+       System.out.println(s1.name); 
+       System.out.println(s1.prog); 
+       s1.updateProgram("CS"); 
+       String prog = s1.accessProgram(); 
+       System.out.println(prog); 
+   } 
 }
