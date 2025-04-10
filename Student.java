25bebd1c5d4ab0918761f69fe55134081d2bd0fe
Abdullah Al Tahsin <qwwwrafid9@gmail.com>
2025-04-10 16:00:27 +0600

    Add files via upload

--- Student.java
+++ Student.java
@@ -1,22 +1,23 @@
-public class Student{
-  public String name;
-  public String prog;
-  
-  public Student(String nam,String pro){
-    name=nam;
-    prog=pro;
-  }
-  
-  public void updateName(String na){
-    name=na;
-  }
-  
-  public void updateProgram(String c){
-    prog=c;
-  }
-  
-  public String accessProgram(){
-    return prog;
-  }
+public class Student {
+    public String emAil = null;
+    public String pasS = null;
+    public String logstat = null;
+    public String nam = " ";
+    public String course = " ";
+    public int iD = 0; 
+
+    public Student(String n1, int id, String cou) {
+        nam = n1;
+        iD = id;
+        course = cou;
+        System.out.println("Student object is created");
+    }
+    
+    public void email(String email) {
+       emAil = email;
+    }
+
+    public void password(String password) {
+        pasS = password;
+    }
 }
-  
