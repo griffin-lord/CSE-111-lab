d1b97e73a8ea2920d7bb593f1475946da9fedb10
Abdullah Al Tahsin <qwwwrafid9@gmail.com>
2025-04-24 10:43:20 +0600

    Add files via upload

--- Dog.java
+++ Dog.java
@@ -1,15 +1,16 @@
-public class Cat extends Animal{
+public class Dog extends Animal{
   public String nam;
   public int ag;
   public String clr;
   public String type;
   
-  public Cat(String nm,int ag,String c3,String c4){
+  public Dog(String nm,int ag,String c3,String c4){
     super(nm,ag,c4);
     clr=c3;
     nam=nm;
   }
   public void makeSound(){
-    System.out.println(clr+" color "+nam+" is meowing");
+    System.out.println(clr+" color "+nam+" is barking");
   }
 }
+  
