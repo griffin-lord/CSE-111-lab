d043cb10715a2bdbe8d671cd129c8c3e8f82114f
Abdullah Al Tahsin <qwwwrafid9@gmail.com>
2025-04-25 20:56:43 +0600

    Add files via upload

---
+++ BBAStudent.java
@@ -0,0 +1,12 @@
+public class BBAStudent extends Student {
+ public BBAStudent() {
+   super();
+  setName("Default");
+  setDepartment("BBA");
+  }
+ public BBAStudent(String name) {
+  super();
+   setName(name);
+  setDepartment("BBA");
+  }
+}
